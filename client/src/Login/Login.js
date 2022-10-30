b002c2123ed37938db19b8345b8df7aaf97c438c
Roshan-jose-a-j <mithunbala18@gmail.com>
2022-10-30 21:48:00 +0530

    Login page changes

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -33,30 +33,58 @@ const Login = () => {
 
 
 
-  return (
-    <div className="login">
-      <div className="lContainer">
-        <input
-          type="text"
-          placeholder="username"
-          id="username"
-          onChange={handleChange}
-          className="lInput"
-        />
-        <input
-          type="password"
-          placeholder="password"
-          id="password"
-          onChange={handleChange}
-          className="lInput"
-        />
-        <button disabled={loading} onClick={handleClick} className="lButton">
-          Login
-        </button>
-        {error && <span>{error.message}</span>}
-      </div>
-    </div>
-  );
-};
+
+    return (
+        <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+            <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
+                <h1 className="text-3xl font-semibold text-center text-purple-700 uppercase">
+                    Sign in
+                </h1>
+                <form className="mt-6">
+                    <div className="mb-2">
+                        <label
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Username
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="username"
+                            id="username"
+                            onChange={handleChange}
+                            className="lInput block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div>
+                    <div className="mb-2">
+                        <label
+                            className="block text-sm font-semibold text-gray-800"
+                        >
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            placeholder="password"
+                            id="password"
+                            onChange={handleChange}
+                            className="lInput block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                        />
+                    </div>
+              
+                    <div className="mt-6">
+                        <button 
+                          disabled={loading} onClick={handleClick}
+                          className="lButton w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
+                         >
+                            Login
+                        </button>
+                        {error && <span>{error.message}</span>}
+                  
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    );
+}
 
 export default Login;
