bf45af77affb90cdb02b310b0ff24a9ef1f37b31
Roshan-jose-a-j <mithunbala18@gmail.com>
2022-11-04 03:57:23 +0530

    Login Page and Landing Page Changes and a New Logo

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -35,12 +35,17 @@ const Login = () => {
 
 
     return (
-        <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
-            <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
-                <h1 className="text-3xl font-semibold text-center text-purple-700 uppercase">
-                    Sign in
+        <div className="relative flex flex-col bg-gray-200 justify-center min-h-screen overflow-hidden">
+            
+            <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-sm ">
+                <img src="images/favicon/android-chrome-192x192.png" className="m-auto"></img>
+                <h1 className="text-2xl pt-10 font-semibold text-center text-black">
+                    Welcome 
                 </h1>
-                <form className="mt-6">
+                <h3 className="text-center pt-2">
+                    Log in to BookYourTrip to Continue
+                </h3>
+                <form className="mt-6 p-4 ">
                     <div className="mb-2">
                         <label
                             className="block text-sm font-semibold text-gray-800"
@@ -49,7 +54,6 @@ const Login = () => {
                         </label>
                         <input
                             type="text"
-                            placeholder="username"
                             id="username"
                             onChange={handleChange}
                             className="lInput block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
@@ -63,14 +67,13 @@ const Login = () => {
                         </label>
                         <input
                             type="password"
-                            placeholder="password"
                             id="password"
                             onChange={handleChange}
                             className="lInput block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
               
-                    <div className="mt-6">
+                    <div className="mt-6 pb-6">
                         <button 
                           disabled={loading} onClick={handleClick}
                           className="lButton w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-purple-700 rounded-md hover:bg-purple-600 focus:outline-none focus:bg-purple-600"
